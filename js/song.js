b4580bfc79be8d5d911eff3ff3e0291d57920a68
Jonathan Baudanza <jon@jonb.org>
2017-02-17 20:40:48 -0800

    first pass at happy birthday

--- js/song.js
+++ js/song.js
@@ -1,4 +1,4 @@
-export const song = [
+const maryHadALittleLamb = [
   [ 'A', 0, 1 ],  // Ma-
   [ 'G', 1, 1 ],  // ry
   [ 'F', 2, 1 ],  // had
@@ -33,3 +33,43 @@ export const song = [
 
   [ 'F', 28, 2 ]  // snow
 ];
+
+// time-signature is 3/4
+const happyBirthday = [
+                   // -silent
+  ['D', 3,   0.5],  // hap-
+  ['D', 3.5, 0.5],  // py
+
+  ['E', 4, 1],      // birth
+  ['D', 5, 1],      // day
+  ['G', 6, 1],      // to
+
+  ['F', 7,     2],  // you
+  ['D', 9,   0.5],  // hap-
+  ['D', 9.5, 0.5],  // py
+
+  ['E', 10, 1],     // birth
+  ['D', 11, 1],     // day
+  ['A', 12, 1],     // to
+
+  ['G', 13,     2], // you
+  ['D', 15,   0.5], // hap-
+  ['D', 15.5, 0.5], // py
+
+  ['D', 16, 1],     // birth     // TODO: This should be an octave higher
+  ['B', 17, 1],     // day
+  ['G', 18, 1],     // dear
+
+  ['F', 19,     1], // blah
+  ['E', 20,     1], // blah
+  ['C', 21,   0.5], // hap-  // TODO: This should be an octave higher
+  ['C', 21.5, 0.5], // py
+
+  ['B', 22, 1],      // birth
+  ['G', 23, 1],      // day
+  ['A', 24, 1],      // to
+
+  ['G', 25, 1]        // you
+];
+
+export const song = happyBirthday;
