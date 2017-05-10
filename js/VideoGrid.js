85ff1fe3e9f0c127c26cfab9afbc3f20549aa9c6
chirgwin <geoff@portchaw.com>
2017-05-10 15:22:33 -0700

    initial move to diatonic video grid

--- js/VideoGrid.js
+++ js/VideoGrid.js
@@ -14,9 +14,10 @@ function Spin(props) {
 
 import "./VideoGrid.scss";
 
-const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-  .map(note => note + "4")
-  .concat(["C5", "C#5", "D5", "D#5"]);
+const notes = ["C3", "D3", "E3", "F3",
+               "G3", "A3", "B3", "C4",
+               "D4", "E4", "F4", "G4",
+               "A4", "B4", "C5", "D5"];
 
 export default class VideoGrid extends React.Component {
   constructor(props) {
