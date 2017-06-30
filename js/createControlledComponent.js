84d242fd92ff31e0925e9fcf39506995e289d0c0
Jonathan Baudanza <jon@jonb.org>
2017-06-30 16:18:19 -0700

    ChooseSongOverlay uses createControlledComponent

---
+++ js/createControlledComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+
+import ReactActions from "./ReactActions";
+
+export default function createControlledComponent(
+  controller,
+  Component,
+  actionNames
+) {
+  return class ControlledComponent extends React.Component {
+    constructor(props) {
+      super();
+      this.props$ = new BehaviorSubject(props);
+      this.state = { current: {} };
+      this.actions = new ReactActions(actionNames);
+    }
+
+    componentWillMount() {
+      const observable$ = controller(
+        this.props$.asObservable(),
+        this.actions.observables
+      );
+      this.subscription = observable$.subscribe(current =>
+        this.setState({ current })
+      );
+    }
+
+    componentWillUnmount() {
+      this.props$.complete();
+      this.actions.completeAll();
+      this.subscription.unsubscribe();
+    }
+
+    render() {
+      return <Component {...this.actions.callbacks} {...this.state.current} />;
+    }
+  };
+}
