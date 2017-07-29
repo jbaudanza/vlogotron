97b37af71a32b81a9160a0a591aa0f3e4bf3a425
Jonathan Baudanza <jon@jonb.org>
2017-07-29 18:04:11 -0400

    Default translations to english

--- js/messages.js
+++ js/messages.js
@@ -1,4 +1,7 @@
 const en = require("messageformat-loader?locale=en&intlSupport=true!json-loader!../lang/en");
 const ko = require("messageformat-loader?locale=ko&intlSupport=true!json-loader!../lang/ko");
 
-module.exports = { en, ko };
+module.exports = {
+  en: en,
+  ko: Object.assign({}, en, ko)
+};
