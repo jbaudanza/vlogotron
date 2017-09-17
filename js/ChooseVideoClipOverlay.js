9b7d5d67bb4a79ddfa9d1f5f2f341d66c5ffde8b
Jonathan Baudanza <jon@jonb.org>
2017-09-17 19:53:14 +0800

    First pass at "Pick" and "play" buttons

--- js/ChooseVideoClipOverlay.js
+++ js/ChooseVideoClipOverlay.js
@@ -2,8 +2,8 @@
 
 import * as React from "react";
 import * as firebase from "firebase";
+import styled from "styled-components";
 
-import Overlay from "./Overlay";
 import createControlledComponent from "./createControlledComponent";
 import combineTemplate from "./combineTemplate";
 
@@ -12,8 +12,12 @@ import { Observable } from "rxjs/Observable";
 import { videoClipById } from "./mediaLoading";
 import type { VideoClipSources } from "./mediaLoading";
 import { videoClipsForSongBoard } from "./database";
-import SynchronizedVideo from "./SynchronizedVideo";
 import audioContext from "./audioContext";
+
+import Overlay from "./Overlay";
+import SynchronizedVideo from "./SynchronizedVideo";
+import PlayButton from "./PlayButton";
+import ActionLink from "./ActionLink";
 
 type OuterProps = {
   songBoardId: string,
@@ -28,24 +32,12 @@ type InnerProps = {
 class CreateVideoClipOverlay extends React.Component<InnerProps> {
   render() {
     return (
-      <Overlay onClose={this.props.onClose}>
+      <StyledOverlay onClose={this.props.onClose}>
+        <h1>Pick a video</h1>
         {this.props.videoClipSources.map((sources, i) => (
-          <SynchronizedVideo
-            key={i}
-            width={100}
-            height={100}
-            videoClipSources={sources}
-            playbackStartedAt={null}
-            playbackParams={{
-              trimStart: 0,
-              trimEnd: 1,
-              playbackRate: 1,
-              gain: 1
-            }}
-            audioContext={audioContext}
-          />
+          <VideoClip key={i} videoClipSources={sources} />
         ))}
-      </Overlay>
+      </StyledOverlay>
     );
   }
 }
@@ -84,6 +76,77 @@ function controller(
     videoClipSources: videoClipSources$
   });
 }
+
+const defaultPlaybackParams = {
+  trimStart: 0,
+  trimEnd: 1,
+  playbackRate: 1,
+  gain: 1
+};
+
+function noop() {}
+
+function VideoClip(props) {
+  return (
+    <VideoWrapper>
+      <StyledPlayButton
+        size={15}
+        isPlaying={false}
+        onClickPlay={noop}
+        onClickPause={noop}
+      />
+      <PickButton>
+        pick
+      </PickButton>
+      <SynchronizedVideo
+        width={100}
+        height={100}
+        videoClipSources={props.videoClipSources}
+        playbackStartedAt={null}
+        playbackParams={defaultPlaybackParams}
+        audioContext={audioContext}
+      />
+    </VideoWrapper>
+  );
+}
+
+const StyledOverlay = styled(Overlay)`
+  .content {
+    width: 500px;
+    height: 500px;
+  }
+
+  video {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+`;
+const VideoWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
+  display: inline-block;
+  position: relative;
+  margin-right: 10px;
+  margin-bottom: 10px;
+`;
+
+const PickButton = styled(ActionLink)`
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  height: 15px;
+  opacity: 1;
+  line-height: 15px;
+`;
+
+const StyledPlayButton = styled(PlayButton)`
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+  svg { display: block; }
+`;
 
 export default createControlledComponent(
   controller,
