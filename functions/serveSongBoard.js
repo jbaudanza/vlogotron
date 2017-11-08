070065bd3d0e91a9d05346dd2c86cd1cd1b828d5
Jonathan Baudanza <jon@jonb.org>
2017-11-08 17:26:08 +0900

    More opengraph tags

--- functions/serveSongBoard.js
+++ functions/serveSongBoard.js
@@ -16,24 +16,38 @@ function createVideoClip(
     return;
   }
 
-  let options;
+  let openGraph;
 
   if (req.path === "/") {
-    options = {
-      title: "Vlogotron: Remix My Face!",
-      url: "https://www.vlogotron.com/",
-      image: "https://www.vlogotron.com/screenshot.png"
+    openGraph = {
+      "og:title": "Vlogotron: Remix My Face!",
+      "og:url": "https://www.vlogotron.com/",
+      "og:image": "https://www.vlogotron.com/screenshot.png"
+    };
+  } else if (req.path === "/songs/-KvXvCxVaAskZ62l5AJr") {
+    openGraph = {
+      "og:title": "Vlogotron: Slide 100bpm",
+      "og:url": "https://www.vlogotron.com" + String(req.path),
+      "og:image": "https://s3-us-west-1.amazonaws.com/www.jonb.org/abay.png",
+      "og:type": "video.other",
+      "fb:app_id": "177263209421488",
+      "og:description": "Abay beatboxes at 100bpm",
+      "og:video:url": "https://s3-us-west-1.amazonaws.com/www.jonb.org/demo-offwhite.mp4",
+      "og:video:secure_url": "https://s3-us-west-1.amazonaws.com/www.jonb.org/demo-offwhite.mp4",
+      "og:video:width": 800,
+      "og:video:height": 800,
+      "og:video:type": "video/mp4"
     };
   } else {
-    options = {
-      title: "Vlogotron",
-      url: "https://www.vlogotron.com" + String(req.path),
-      image: null
+    openGraph = {
+      "og:title": "Vlogotron",
+      "og:url": "https://www.vlogotron.com" + String(req.path),
+      "og:image": null
     };
   }
 
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(renderTemplate(options));
+  res.end(renderTemplate({openGraph}));
 }
 
 module.exports = createVideoClip;
