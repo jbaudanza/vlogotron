e0e86ee81ca4223293f2778469b6204f6dec6c11
Jonathan Baudanza <jon@jonb.org>
2018-01-03 17:36:44 -0800

    Add VideoCell to storybook

--- js/messages.js
+++ js/messages.js
@@ -1,7 +1,7 @@
 const en = require("messageformat-loader?locale=en&intlSupport=true!json-loader!../lang/en");
 const ko = require("messageformat-loader?locale=ko&intlSupport=true!json-loader!../lang/ko");
 
-module.exports = {
+export default {
   en: en,
   ko: Object.assign({}, en, ko)
 };
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,79 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import VideoCell from "../js/VideoCell";
+import messages from "../js/messages";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+class WithUserMedia extends React.Component {
+  constructor() {
+    super();
+    this.state = { mediaStream: null, error: null }
+  }
+
+  componentDidMount() {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(
+      (mediaStream) => this.setState({mediaStream}),
+      (error) => this.setState({error}),
+    )
+  }
+
+  render() {
+    if (this.state.mediaStream != null) {
+      return this.props.render(this.state.mediaStream);
+    }
+
+    if (this.state.error) {
+      return <div>{this.state.error}</div>;
+    }
+
+    return <div>Waiting for user media</div>;
+  }
+}
+
+class WithMessages extends React.Component {
+  getChildContext() {
+    return {
+      messages: messages['en'],
+      locale: 'en'
+    };
+  }
+
+  render() {
+    return this.props.children;
+  }
+};
+
+WithMessages.childContextTypes = {
+  messages: PropTypes.object,
+  locale: PropTypes.string
+};
+
+storiesOf('VideoCell', module)
+  .add('while recording', () => {
+    return (
+      <WithMessages>
+        <WithUserMedia render={(mediaStream) => (
+          <VideoCell
+            onStopRecording={action('stop recording')}
+            durationRecorded={123}
+            mediaStream={mediaStream} />
+        )} />
+      </WithMessages>
+    )
+  })
+  .add('with countdown', () => {
+    return (
+      <WithMessages>
+        <WithUserMedia render={(mediaStream) => (
+          <VideoCell
+            onStopRecording={action('stop recording')}
+            countdown={3}
+            mediaStream={mediaStream} />
+        )} />
+      </WithMessages>
+    )
+  })
